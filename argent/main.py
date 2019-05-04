273e369838b393bcae2deac650096a2ed73b0ed8
Robert Fasano <robert.j.fasano@colorado.edu>
2019-05-04 10:47:11 -0600

    Build gui prototype

---
+++ argent/main.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+from flask import render_template
+import json
+import threading
+import numpy as np
+from pint import UnitRegistry
+ureg = UnitRegistry()
+Q_ = ureg.Quantity
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+
+@app.route("/get")
+def get():
+    message = "Hello, World"
+    return message
+
+@app.route("/loopback", methods=['POST'])
+def loopback():
+    return json.dumps(request.get_json())
+
+@app.route("/convert/<text>", methods=['GET'])
+def convert(text):
+    if text == '':
+        return ''
+    duration = Q_(text)
+    if str(duration.units) == 'dimensionless':
+        duration = Q_('{} {}'.format(duration.magnitude, 's'))    # assume base unit if none is passed
+    cleaned_value = duration.to_compact()                   # auto-convert units for compact view
+    cleaned_value = np.round(cleaned_value, 9)              # round to 9 digits to remove numerical rounding errors in Python
+    return '{:~}'.format(cleaned_value)
+
+@app.route("/start", methods=['POST'])
+def start():
+    sequence = request.get_json()['payload']
+    with open('sequence.json', 'w') as file:
+        json.dump(sequence, file)
+    return ''
+
+
+
+# run the application
+if __name__ == "__main__":
+#    thread = threading.Thread(target=app.run)
+#    thread.start()
+    app.run(debug=True, port=8051)
