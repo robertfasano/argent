34842c60aaa2dd6019c114761eab05561f6a6fc9
Robert Fasano <robert.j.fasano@colorado.edu>
2019-07-01 17:24:17 -0600

    Add delay between server starting and querying

--- argent/tests/test_server.py
+++ argent/tests/test_server.py
@@ -6,5 +6,6 @@ def test_host():
     from threading import Thread
     server = Thread(target=host)
     server.start()
+    time.sleep(30)
     assert requests.get('http://127.0.0.1:5000/test').text == 'Hello, world!'
     requests.post('http://127.0.0.1:5000/shutdown')
