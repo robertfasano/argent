859364b1b32e54fde10dad071452ea2737466204
Robert Fasano <robert.j.fasano@colorado.edu>
2019-07-11 18:24:20 -0600

    Overhaul code generation
    Near-rewrite of code generation allowing simpler command-line usage, initial (pre-loop) states, simple access of channels across multiple of the same card type, and reduction of code complexity

--- argent/base_experiment.py
+++ argent/base_experiment.py
@@ -1,43 +1,9 @@
 from artiq.experiment import *
 import numpy as np
 
-from generated.preloop import preloop
+from generated.run import run
+from generated.build import build
 
 class Sequencer(EnvExperiment):
-    def build(self):
-        self.setattr_device("core")
-        self.setattr_device("scheduler")
-        self.setattr_device("core_dma")
-        self.setattr_device("urukul0_cpld")  #4 Channels of DDS
-        self._dds = []
-        for i in range(4):
-            self.setattr_device('urukul0_ch{}'.format(i))
-            dev = getattr(self, 'urukul0_ch{}'.format(i))
-            self._dds.append(dev)
-
-        self._ttls = []
-        for i in range(16):
-            self.setattr_device("ttl%i"%i)
-            dev = getattr(self, "ttl%i"%i)
-            self._ttls.append(dev)
-
-        self.setattr_device('zotino0')          # 32 channel DAC
-        self.setattr_device('sampler0')         # 8 channel ADC
-
-    @kernel
-    def initialize_kernel(self):
-        self.core.reset()
-        self.core.break_realtime()
-        self.sampler0.init()
-        self.core.break_realtime()
-        self.zotino0.init()
-        self.core.break_realtime()
-        self.urukul0_cpld.init()
-        for i in range(4):
-            self._dds[i].init()         # initialize channel
-            self._dds[i].sw.on()        # open rf switch
-        delay(10*ms)
-
-    @kernel
-    def run(self):
-        preloop(self)
+    build = build
+    run = run
--- argent/generator.py
+++ argent/generator.py
@@ -1,119 +1,243 @@
 import os
-from config import device_db
+import re
+from config import devices, device_db
 
-def run_sequence(sequence):
-    write_preloop(sequence)
-    write_loop(sequence)
-    os.system(f'start "" cmd /k "cd /argent/argent/ & call activate artiq-4 & artiq_run base_experiment.py --device-db={device_db}"')
+def write_build():
+    code = f"""
+def build(self):
+    self.setattr_device("core")
+    self.setattr_device("scheduler")
+    self.setattr_device("core_dma")
+    ''' Initialize DDS '''
+    for urukul in {devices['dds']}:
+        self.setattr_device("%s_cpld"%(urukul))  #4 Channels of DDS
+        for i in range(4):
+            self.setattr_device('%s_ch%i'%(urukul, i))
 
-def write_preloop(sequence):
-    ''' Write base code '''
-    code = ''
-    code += 'from artiq.experiment import *\n'
-    code += 'from generated.loop import loop\n'
-    code += '@kernel\n'
-    code += 'def preloop(self):\n'
+    ''' Initialize TTL '''
+    self._ttls = []
+    for ttl in {devices['ttl']}:
+        self.setattr_device('ttl%s'%ttl)
+        self._ttls.append(getattr(self, 'ttl%s'%ttl))
 
-    code += '\tself.initialize_kernel()\n'
-    code += '\tfor ttl in self._ttls:\n'
-    code += '\t\tttl.output()\n'
-    code += '\t\tttl.off()\n'
+    ''' Initialize DAC '''
+    for dac in {devices['dac']}:
+        self.setattr_device(dac)          # 32 channel DAC
 
+    ''' Initialize ADC '''
+    self._adcs = []
+    for adc in {devices['adc']}:
+        self.setattr_device(adc)         # 8 channel ADC
+        self._adcs.append(getattr(self, adc))
+            """
+
+    with open('generated/build.py', 'w') as file:
+        file.write(code)
+
+
+def write_run(sequence, initial=None):
     ''' Prepare data array '''
     adc_delay = 1e-3
     N_samples = []
     for i in range(len(sequence)):
         N_samples.append(int(sequence[i]['duration']/adc_delay))
-    code += '\tadc_delay={}\n'.format(adc_delay)
-    code += '\tN_samples={}\n'.format(N_samples)
-    code += '\tdata = [[[0 for ch in range(8)] for n in range(N_samples[i])] for i in range({})]\n'.format(len(sequence))
-    code += '\tself.core.break_realtime()\n'
 
-    ''' Pre-set DDS '''
-    step = sequence[0]['DDS']
-    code += '\tfreqs = {}\n'.format([x['frequency'] for x in step])
-    code += '\tatts = {}\n'.format([x['attenuation'] for x in step])
+    ''' Write base code '''
+    code = f"""
+from artiq.experiment import *
+from generated.loop import loop
+@kernel
+def run(self):
+    print('Running ARTIQ sequence.')
+    adc_delay = {adc_delay}
+    N_samples = {N_samples}
+    data = [[[0 for ch in range(8)] for n in range(N_samples[i])] for i in range({len(sequence)})]
 
-    code += '\tfor i in range(len(self._dds)):\n'
-    code += '\t\tdelay(10*ns)\n'
-    code += '\t\tself._dds[i].set(freqs[i]*Hz)\n'
-    code += '\t\tdelay(10*ns)\n'
-    code += '\t\tself._dds[i].set_att(atts[i])\n'
+    ''' Initialize kernel '''
+    self.core.reset()
+    self.core.break_realtime()
+    """
 
-    # for d in range(len(step)):
-    #     ''' Frequency updates '''
-    #     code += '\tdelay(10*ns)\n'
-    #     code += '\tself.urukul0_ch{}.set({}*Hz)\n'.format(d, step[d]["frequency"])
-    #     code += '\tdelay(10*ns)\n'
-    #     ''' Attenuation updates'''
-    #
-    #     code += '\tself.urukul0_ch{}.set_att({})\n'.format(d, step[d]["attenuation"])
+    ''' DAC initialization '''
+    for dac in devices['dac']:
+        code += f"self.{dac}.init()\n"
 
-    code += '\tself.core.break_realtime()\n'
-    code += '\twhile True:\n'
-    code += '\t\tdata=loop(self, data, adc_delay, N_samples)\n'
+    ''' DDS initialization '''
+    for dds in devices['dds']:
+        code += f"""
+    self.{dds}_cpld.init()"""
+    for ch in range(4):
+        code += f"""
+    self.{dds}_ch{ch}.init()
+    self.{dds}_ch{ch}.sw.on()"""
 
-    # code += '\t\tprint(data)'
+
+    ''' Set TTLs to output and initialize to low '''
+    code += f"""
+
+    for ttl in self._ttls:
+        ttl.output()
+        ttl.off()
+    """
+
+    ''' Initialize DACs to 0 V '''
+    for dac in devices['dac']:
+        code += f"""
+    delay(2*ms)
+    self.{dac}.set_dac([0.0]*32, list(range(32)))
+    self.core.break_realtime()\n\n"""
+
+    ''' Write initial step '''
+    if initial is not None:
+        code += "    '''Prepare initial state'''\n"
+        code += write_initial(initial)
+
+        ''' Since TTLs are stateful, we need to manually switch off any TTLs which were turned on
+            in the initial step but will be off in the first step of the sequence '''
+        code += "\n    '''Prepare TTLs for loop'''\n"
+        if 'TTL' in initial:
+            if 'TTL' in sequence[0]:
+                next_ttls = sequence[0]['TTL']
+            else:
+                next_ttls = []
+            to_turn_off = [ttl for ttl in initial['TTL'] if ttl not in next_ttls]
+            for ttl in to_turn_off:
+                code += f"""    self.ttl{ttl}.off()\n"""
+
+
+    code += f"""
+    while True:
+        data=loop(self, data, adc_delay, N_samples)
+    """
 
     ''' Finish and save to file '''
-    with open('generated/preloop.py', 'w') as file:
+    with open('generated/run.py', 'w') as file:
         file.write(code)
+
+def write_initial(step):
+    code = ''
+    ''' Write TTL events '''
+    to_turn_on = [x for x in step['TTL']]
+    code += '    with parallel:\n'
+    for ch in to_turn_on:
+        code += '        self.ttl{}.on()\n'.format(ch)
+
+    code += '        delay({:g}*s)\n'.format(step['duration'])
+
+    ''' Write DAC events '''
+    if 'DAC' in step:
+        for dac in devices['dac']:
+            devnum = dac.split('zotino')[1]
+            channels = []
+            voltages = []
+            for ch in step['DAC']:
+                dev = re.split('(\d+)',ch)[0]
+                chnum = re.split('(\d+)',ch)[1]
+                if dev == devnum:
+                    channels.append(int(chnum))
+                    voltages.append(float(step['DAC'][ch]))
+            code += f"        self.zotino{devnum}.set_dac({voltages}, {channels})\n"
+
+    ''' Write DDS events '''
+    if 'DDS' in step:
+        for ch in step['DDS']:
+            dev = re.split('(\d+)',ch)[0]
+            chnum = re.split('(\d+)',ch)[1]
+            written_sequential_block = False
+
+            if 'frequency' in step['DDS'][ch]:
+                freq = float(step['DDS'][ch]['frequency'])
+                if not written_sequential_block:
+                    code += '        with sequential:\n'
+                    written_sequential_block = True
+                code += f'            self.urukul{dev}_ch{chnum}.set({freq}*Hz)\n'
+                code += '            delay(10*ns)\n'
+
+            if 'attenuation' in step['DDS'][ch]:
+                att = float(step['DDS'][ch]['attenuation'])
+                if not written_sequential_block:
+                    code += '\t\twith sequential:\n'
+                    written_sequential_block = True
+                code += f'            self.urukul{dev}_ch{chnum}.set_att({att})\n'
+                code += '            delay(10*ns)\n'
+
+    return code
+
+def write_step(step, last_step, i):
+    code = ''
+    ''' Write TTL events '''
+    to_turn_on = [x for x in step['TTL'] if x not in last_step['TTL']]
+    to_turn_off = [x for x in last_step['TTL'] if x not in step['TTL']]
+    if i == 0:
+        to_turn_on = [x for x in step['TTL']]
+    code += '\twith parallel:\n'
+    for ch in to_turn_on:
+        code += '\t\tself.ttl{}.on()\n'.format(ch)
+
+    for ch in to_turn_off:
+        code += '\t\tself.ttl{}.off()\n'.format(ch)
+    code += '\t\tdelay({:g}*s)\n'.format(step['duration'])
+
+    ''' Write DAC events '''
+    if 'DAC' in step:
+        for dac in devices['dac']:
+            devnum = dac.split('zotino')[1]
+            channels = []
+            voltages = []
+            for ch in step['DAC']:
+                dev = re.split('(\d+)',ch)[0]
+                chnum = re.split('(\d+)',ch)[1]
+                if dev == devnum:
+                    channels.append(int(chnum))
+                    voltages.append(float(step['DAC'][ch]))
+            code += f"\t\tself.zotino{devnum}.set_dac({voltages}, {channels})\n"
+
+    ''' Write DDS events '''
+    if 'DDS' in step:
+        for ch in step['DDS']:
+            dev = re.split('(\d+)',ch)[0]
+            chnum = re.split('(\d+)',ch)[1]
+            written_sequential_block = False
+
+            if 'frequency' in step['DDS'][ch]:
+                freq = float(step['DDS'][ch]['frequency'])
+                if not written_sequential_block:
+                    code += '\t\twith sequential:\n'
+                    written_sequential_block = True
+                code += f'\t\t\tself.urukul{dev}_ch{chnum}.set({freq}*Hz)\n'
+                code += '\t\t\tdelay(10*ns)\n'
+
+            if 'attenuation' in step['DDS'][ch]:
+                att = float(step['DDS'][ch]['attenuation'])
+                if not written_sequential_block:
+                    code += '\t\twith sequential:\n'
+                    written_sequential_block = True
+                code += f'\t\t\tself.urukul{dev}_ch{chnum}.set_att({att})\n'
+                code += '\t\t\tdelay(10*ns)\n'
+
+
+    # if 'ADC' in sequence[i]:
+
+    # ''' Write ADC events '''
+    # if sequence[i]['ADC'] != []:
+    #     code += '\t\tfor j in range(len(N_samples)):\n'
+    #     code += '\t\t\twith parallel:\n'
+    #     code += '\t\t\t\tself.sampler0.sample_mu(data[{}][j])\n'.format(i)
+    #     code += '\t\t\t\tdelay(adc_delay)\n'
+
+    return code
 
 def write_loop(sequence):
     ''' Write base code '''
     code = ''
     code += 'from artiq.experiment import *\n'
     code += '@kernel\n'
-    code += 'def loop(self, data, adc_delay, N_samples):\n'
+    code += 'def loop(self, data, adc_delay, N_samples, initial=False):\n'
 
-    ''' Write TTL events '''
+    dac_state = [0]*32
+
     for i in range(len(sequence)):
-        step = sequence[i]['TTL']
-        last_step = sequence[i-1]['TTL']
-        to_turn_on = [x for x in step if x not in last_step]
-        to_turn_off = [x for x in last_step if x not in step]
-        if i == 0:
-            to_turn_on = [x for x in step]
-        code += '\twith parallel:\n'
-        for ch in to_turn_on:
-            code += '\t\tself.ttl{}.on()\n'.format(ch)
-        for ch in to_turn_off:
-            code += '\t\tself.ttl{}.off()\n'.format(ch)
-        code += '\t\tdelay({:g}*s)\n'.format(sequence[i]['duration'])
-
-        ''' Write DAC events '''
-        step = sequence[i]['DAC']
-        code += '\t\tself.zotino0.set_dac({}, {})\n'.format(step, list(range(len(step))))
-
-
-        ''' Write DDS events '''
-        step = sequence[i]['DDS']
-        last_step = sequence[i-1]['DDS']
-
-        for d in range(len(step)):
-            written_sequential_block = False
-            ''' Frequency updates '''
-            if step[d]['frequency'] != last_step[d]['frequency']:
-                if not written_sequential_block:
-                    code += '\t\twith sequential:\n'
-                    written_sequential_block = True
-                code += '\t\t\tself.urukul0_ch{}.set({}*Hz)\n'.format(d, step[d]["frequency"])
-                code += '\t\t\tdelay(10*ns)\n'
-
-            ''' Attenuation updates'''
-            if step[d]['attenuation'] != last_step[d]['attenuation']:
-                if not written_sequential_block:
-                    code += '\t\twith sequential:\n'
-                    written_sequential_block = True
-                code += '\t\t\tself.urukul0_ch{}.set_att({})\n'.format(d, step[d]["attenuation"])
-                code += '\t\t\tdelay(10*ns)\n'
-
-        ''' Write ADC events '''
-        if sequence[i]['ADC'] != []:
-            code += '\t\tfor j in range(len(N_samples)):\n'
-            code += '\t\t\twith parallel:\n'
-            code += '\t\t\t\tself.sampler0.sample_mu(data[{}][j])\n'.format(i)
-            code += '\t\t\t\tdelay(adc_delay)\n'
+        code += write_step(sequence[i], sequence[i-1], i)
 
     ''' Finish and save to file '''
     code += '\treturn data'
@@ -122,3 +246,17 @@ def write_loop(sequence):
         file.write(code)
 
     return code
+
+def run_sequence(sequence, initial=None):
+    write_build()
+    write_run(sequence, initial=initial)
+    write_loop(sequence)
+    os.system(f'start "" cmd /k "cd /argent/argent/ & call activate artiq-4 & artiq_run base_experiment.py --device-db={device_db}"')
+
+if __name__ == '__main__':
+    initial_state = {'duration': 100e-3, 'TTL': ['A1'], 'DAC': {'A0': 5}, 'DDS': {'A0': {'frequency':1e6}}}
+    sequence = [{'duration': 10e-3, 'TTL': ['A0'], 'DAC': {'A0': 1}, 'DDS': {'A0': {'attenuation':0}}},
+                {'duration': 10e-3, 'TTL': ['A2'], 'DAC': {'A0': 2}, 'DDS': {'A0': {'attenuation':31}}}
+    ]
+    print(sequence)
+    run_sequence(sequence, initial=initial_state)
