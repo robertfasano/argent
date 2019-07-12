a5ed228067d325bb7ba8eaa00be50be4c438c788
Robert Fasano <robert.j.fasano@colorado.edu>
2019-07-11 18:22:27 -0600

    Allow multiple of the same card type
    Cards are now labeled with an alphabetical identifier, e.g. urukulA, with channels labeled numerically.

--- argent/config.py
+++ argent/config.py
@@ -1,7 +1,19 @@
+## enter numerical channel numbers for TTL, ADC, DAC, and DDS channels.
 channels = {"TTL": list(range(0,8)),
             "ADC": list(range(0,8)),
             "DAC": list(range(0,32)),
             "DDS": list(range(0,4))
+}
+
+''' Enter names of i/o devices as they appear in device_db.py, with the device
+    name prefixes removed. For example, if you have a device 'ttlA0', add
+    'A0' to the 'ttl' field.
+'''
+devices = {
+            'ttl': [f'A{ch}' for ch in range(0,8)],
+            'adc': ['sampler0'],
+            'dac': ['zotinoA'],
+            'dds': ['urukulA']
 }
 
 device_db = 'C:/argent/argent/device_db.py'
