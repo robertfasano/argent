9c09acd0deaa3b579e08b5c09e6e34e68c891ddc
Robert Fasano <robert.j.fasano@colorado.edu>
2019-07-16 22:47:46 -0600

    Add DDSTable

--- argent/gui.py
+++ argent/gui.py
@@ -2,7 +2,7 @@ import json
 import os
 from PyQt5.QtWidgets import QHBoxLayout, QFileDialog, QTabWidget
 from sciQt import Dashboard, IconButton
-from sciQt.widgets import DACTable, TimingTable, TTLTable
+from sciQt.widgets import DACTable, TimingTable, TTLTable, DDSTable
 from argent.generator import run_sequence
 from argent.config import sequences_path
 
@@ -19,13 +19,18 @@ class GUI(Dashboard):
         save_path = 'C:/sciQt/sciQt/resources/icons/content-save-outline.svg'
         load_path = 'C:/sciQt/sciQt/resources/icons/outline-folder_open-24px.svg'
 
-        button_layout.addWidget(IconButton(start_path, lambda: run_sequence(self.timing_table.get_sequence())))
+        # button_layout.addWidget(IconButton(start_path, lambda: run_sequence(self.timing_table.get_sequence())))
+        button_layout.addWidget(IconButton(start_path, lambda: print(self.timing_table.get_sequence())))
+
         button_layout.addWidget(IconButton(save_path, self.save))
         button_layout.addWidget(IconButton(load_path, self.load))
         button_layout.addStretch()
         self.DACs = [f'A{i}' for i in range(32)]
+        self.DDS = [f'A{i}' for i in range(4)]
+
         ttls =  [f'A{i}' for i in range(8)]
-        sequence = [{'duration': 0.2, 'TTL': ['A0'], 'DAC': {'A1': 1}}, {'duration': 0.5, 'TTL': ['A1']}]
+        sequence = [{'duration': 0.2, 'TTL': ['A0'], 'DAC': {'A1': 1}, 'DDS': {'A0': {'frequency': 400}, 'A1': {'attenuation': 2}}},
+                    {'duration': 0.5, 'TTL': ['A1'], 'DDS': {'A0': {'frequency': 300, 'attenuation': 3}}}]
 
         self.timing_table = TimingTable(sequence)
         self.timing_table.model().columnsInserted.connect(lambda: self.resize(self.timing_table.sizeHint()))
@@ -35,11 +40,15 @@ class GUI(Dashboard):
         self.tabs = QTabWidget()
         self.ttl_table = TTLTable(self.timing_table, ttls)
         self.dac_table = DACTable(self.timing_table, self.DACs)
+        self.dds_table = DDSTable(self.timing_table, self.DDS)
+
         self.tabs.addTab(self.ttl_table, 'TTL')
         self.tabs.addTab(self.dac_table, 'DAC')
+        self.tabs.addTab(self.dds_table, 'DDS')
+
         self.layout.addLayout(button_layout)
         self.layout.addWidget(self.tabs)
-        
+
         self.resize(self.timing_table.sizeHint())
 
     def load(self):
