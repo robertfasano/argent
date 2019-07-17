812896e0afd625c9140a1fa61ec4f5c718629c39
Robert Fasano <robert.j.fasano@colorado.edu>
2019-07-16 18:19:41 -0600

    Reimplement TTLTable using master TimingTable; add DACTable

--- argent/gui.py
+++ argent/gui.py
@@ -1,11 +1,11 @@
-from sciQt import Dashboard, TTLTable, IconButton
-from PyQt5.QtWidgets import QHBoxLayout, QFileDialog
-from PyQt5.QtCore import QSize
-
-from generator import run_sequence
-from argent.config import sequences_path
 import json
 import os
+from PyQt5.QtWidgets import QHBoxLayout, QFileDialog, QTabWidget
+from sciQt import Dashboard, IconButton
+from sciQt.widgets import DACTable, TimingTable, TTLTable
+from argent.generator import run_sequence
+from argent.config import sequences_path
+
 
 class GUI(Dashboard):
     def __init__(self):
@@ -19,18 +19,28 @@ class GUI(Dashboard):
         save_path = 'C:/sciQt/sciQt/resources/icons/content-save-outline.svg'
         load_path = 'C:/sciQt/sciQt/resources/icons/outline-folder_open-24px.svg'
 
-        button_layout.addWidget(IconButton(start_path, lambda: run_sequence(self.ttl_table.get_sequence())))
+        button_layout.addWidget(IconButton(start_path, lambda: run_sequence(self.timing_table.get_sequence())))
         button_layout.addWidget(IconButton(save_path, self.save))
         button_layout.addWidget(IconButton(load_path, self.load))
         button_layout.addStretch()
-        ttls =  [f'A{i}' for i in range(0,8)]
-        sequence = [{'duration': 0.2, 'TTL': ['A0']}, {'duration': 0.5, 'TTL': ['A1']}]
-        self.ttl_table = TTLTable(ttls, sequence=sequence)
+        self.DACs = [f'A{i}' for i in range(32)]
+        ttls =  [f'A{i}' for i in range(8)]
+        sequence = [{'duration': 0.2, 'TTL': ['A0'], 'DAC': {'A1': 1}}, {'duration': 0.5, 'TTL': ['A1']}]
 
+        self.timing_table = TimingTable(sequence)
+        self.timing_table.model().columnsInserted.connect(lambda: self.resize(self.timing_table.sizeHint()))
+        self.timing_table.model().columnsRemoved.connect(lambda: self.resize(self.timing_table.sizeHint()))
 
+        self.header = self.timing_table.horizontalHeader()
+        self.tabs = QTabWidget()
+        self.ttl_table = TTLTable(self.timing_table, ttls)
+        self.dac_table = DACTable(self.timing_table, self.DACs)
+        self.tabs.addTab(self.ttl_table, 'TTL')
+        self.tabs.addTab(self.dac_table, 'DAC')
         self.layout.addLayout(button_layout)
-        self.layout.addWidget(self.ttl_table)
-        self.ttl_table.resizeToFit()
+        self.layout.addWidget(self.tabs)
+        
+        self.resize(self.timing_table.sizeHint())
 
     def load(self):
         filename = QFileDialog.getOpenFileName(self, 'Load sequence', sequences_path)[0]
@@ -38,17 +48,14 @@ class GUI(Dashboard):
             return
         with open(filename) as file:
             sequence = json.load(file)
-        self.ttl_table.set_sequence(sequence)
+        self.timing_table.set_sequence(sequence)
 
     def save(self):
         filename = QFileDialog.getSaveFileName(self, 'Save sequence', sequences_path)[0]
         if filename == '':
             return
         with open(filename, 'w') as file:
-            json.dump(self.ttl_table.get_sequence(), file)
-
-    def sizeHint(self):
-        return self.ttl_table.sizeHint()  #+ QSize(0, self.ttl_table.sizeHint().height())
+            json.dump(self.timing_table.get_sequence(), file)
 
 if __name__ == '__main__':
     dashboard = GUI()
