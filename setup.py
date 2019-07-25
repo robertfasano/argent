23f4ec8e008d76af11c9e4b36311f40955196804
Robert Fasano <robert.j.fasano@colorado.edu>
2019-07-25 17:37:27 -0600

    Ensure copying of config.yml and other non-Python files

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup
 from setuptools import find_packages
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+extra_files = package_files('argent')
 
 setup(
     name='argent',
@@ -10,5 +19,6 @@ setup(
     packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
+    package_data={'': extra_files},
     install_requires=['pyyaml>=5.1.1']
 )
