a9a50fd50ffc021497f31528b58ea84f0ac82a59
Robert Fasano <robert.j.fasano@colorado.edu>
2020-03-06 23:55:51 -0700

    Add icon and change title

--- argent/webapp.py
+++ argent/webapp.py
@@ -1,8 +1,14 @@
 from flask import Flask, request
-from flask import render_template
+from flask import render_template, send_from_directory
+import os
 
 def host(addr='127.0.0.1', port=8051):
     app = Flask(__name__)
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'),
+                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
     @app.route("/")
     def hello():
