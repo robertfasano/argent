0edf6f9b0bbcfa2717ea3e3367f8c85e17a36985
Robert Fasano <robert.j.fasano@colorado.edu>
2020-03-08 11:11:42 -0600

    Add optional controlled usage by passing scale and setScale props; preserve precision when scaling user inputs.

--- argent/static/js/ScaledInput.jsx
+++ argent/static/js/ScaledInput.jsx
@@ -4,13 +4,63 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 
+function precisionOf(n) {
+  // Returns the number of significant figures in a number or string.
+  n = n.toString()
 
-export default function ScaledInput(props) {
-  const [scale, setScale] = React.useState(1)
-  const [displayValue, setDisplayValue] = React.useState(props.value)
+  // remove trailing zeros with no decimal point
+  if (!n.includes('.')) {
+    while (true) {
+      let char = n.slice(-1)
+      if (char == '0') {
+        n = n.substring(0, n.length - 1)
+      }
+      else {
+        break
+      }
+    }
+  }
 
-  React.useEffect(scaleInitialValue, [])
+  // remove leading zeros
+  while (true) {
+    let char = n.charAt(0)
+    if (char == '0' || char == '.') {
+      n = n.slice(1)
+    }
+    else {
+      break
+    }
+  }
 
+  // remove decimal point
+  n = n.replace('.', '')
+
+  return n.length
+}
+
+function preciseString(number, precision) {
+  // Converts a number into a string representation with the desired precision
+  return parseFloat(number.toPrecision(precision)).toString()
+}
+
+function ScaledInput(props) {
+  const [scale, setScale] = [props.scale, props.setScale]
+  const [displayValue, setDisplayValue] = React.useState((props.value/scale).toString())
+
+  syncDisplayValue()
+
+  function syncDisplayValue() {
+    // Because the displayValue is a separate state from props.value, they can
+    // desynchronize if props.value is changed from outside this component. This
+    // function checks for this and resynchronizes if necessary.
+    if (props.value == '') {
+      return
+    }
+    if (props.value / scale != displayValue) {
+      let newValue = preciseString(props.value/scale, precisionOf(props.value))
+      setDisplayValue(newValue)
+    }
+  }
   function scaleInitialValue() {
     // Set the initial unit based on the passed value. Examples:
     //    10 -> 10 V
@@ -36,24 +86,28 @@ export default function ScaledInput(props) {
       expof10 += 3*sign
     }
 
-    const scale = 10**expof10
-    if (scaleFactors.includes(scale)) {
-      setScale(10**expof10)
-      setDisplayValue(scaledValue)
+    const factor = 10**expof10
+    if (scaleFactors.includes(factor)) {
+      setScale(factor)
     }
   }
 
-  function onChange(value, scale_value) {
-    if (scale_value == null){
-      scale_value = scale
+  function onChange(value, scaleValue) {
+    if (scaleValue == null){
+      scaleValue = scale
     }
-    props.onChange(value*scale_value)      // send scaled value to passed callback
-    setDisplayValue(value)                 // display unscaled value
+    let newValue = preciseString(value*scaleValue, precisionOf(value))
+    props.onChange(newValue)
+    setDisplayValue(value)
+
   }
 
-  function changeUnits(scale) {
-    setScale(scale)
-    onChange(displayValue, scale)
+  function changeUnits(newScale) {
+    let oldScale = scale
+    let newValue = preciseString(props.value*newScale/oldScale, precisionOf(displayValue))
+    props.onChange(newValue)
+    setScale(newScale)
+
   }
 
   return (
@@ -73,7 +127,15 @@ export default function ScaledInput(props) {
                               </Select>
                              </InputAdornment>
              }}
-
       />
   )
+}
+
+export default function ScaledInputWrapper(props) {
+  if (props.scale == null) {
+    const [scale, setScale] = React.useState(1)
+    var newProps = {...props, scale, setScale}
+    return ScaledInput(newProps)
+  }
+  return ScaledInput(props)
 }
