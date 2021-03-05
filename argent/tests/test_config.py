45cb1345122ebcc67a165f4a80fbba94a12015f6
Robert Fasano <robert.j.fasano@colorado.edu>
2021-03-05 12:00:19 -0700

    Remove unused/obsolete files

--- argent/tests/test_config.py
+++
@@ -1,9 +0,0 @@
-from argent import Configurator
-
-def test_update_config():
-    old_path, = Configurator.load('sequences_path')
-    Configurator.update('sequences_path', 'C:/')
-
-    assert Configurator.load('sequences_path')[0] == 'C:/'
-
-    Configurator.update('sequences_path', old_path)
--- argent/tests/test_generator.py
+++
@@ -1,7 +0,0 @@
-from argent.generator import Generator
-
-def test_code_generator():
-    sequence = [{'sequence': [{'duration': 10e-3, 'TTL': ['A0'], 'DAC': {'A0': 1}, 'DDS': {'A0': {'attenuation':0}}},
-                {'duration': 10e-3, 'TTL': ['A2'], 'DAC': {'A0': 2}, 'DDS': {'A0': {'attenuation':31}}}
-    ]}]
-    Generator(sequence)
