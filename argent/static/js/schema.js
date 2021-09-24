7b96ca5ad861bc54ae7b25485606b50d1577c68b
Robert Fasano <robert.j.fasano@colorado.edu>
2021-09-24 11:15:15 -0600

    Change default timestep duration to 1 ms

--- argent/static/js/schema.js
+++ argent/static/js/schema.js
@@ -25,7 +25,7 @@ export function fill (sequence, channels) {
 
 export function defaultTimestep (channels) {
   const defaultTimestep = {
-    duration: '1 s',
+    duration: '1 ms',
     ttl: {},
     dac: {},
     dds: {},
