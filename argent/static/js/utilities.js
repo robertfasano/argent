667e0c5474af64742d9ee7a88ca00e34bd23c7a1
Fasano <rjf2@NIST.GOV>
2022-09-07 11:40:09 -0600

    Bugfix: check array lengths for shallow comparison

--- argent/static/js/utilities.js
+++ argent/static/js/utilities.js
@@ -19,6 +19,7 @@ export function post (url, payload = {}, callback = (response) => null) {
 }
 
 export const arrayShallowEqual = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false
   let isEqual = true
   for (const i in arr1) {
     if (arr1[i] !== arr2[i]) isEqual = false
